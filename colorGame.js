4537c2b8d8dee794df264290df38ffb78ae052d4
Daniel Esparza <15037512+d-Esparza19@users.noreply.github.com>
2020-08-27 12:23:56 -0500

    Color selection is now dynamic

--- colorGame.js
+++ colorGame.js
@@ -1,18 +1,15 @@
-var colors = ["rgb(255, 0, 0)",
-"rgb(255, 255, 0)",
-"rgb(0, 255, 255)",
-"rgb(0, 0, 255)",
-"rgb(255, 255, 0)",
-"rgb(255, 0, 255)"]
+var colors = generateRandomColors(6);
 
 // Select all display squares
 var squares = document.querySelectorAll(".square");
 // Assign the game winning color
-var pickedColor = colors[3];
+var pickedColor = pickedColor();
 // select the span in the title
 var colorDisplay = document.getElementById("colorDisplay");
 // select the message ID
 var messageDisplay =document.querySelector("#message");
+//Select the header for victory color
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -29,6 +26,7 @@ for(var i=0; i < squares.length; i++){
         if(clickedColor == pickedColor){
             messageDisplay.textContent = "Correct!"
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         }else{
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again!"
@@ -42,4 +40,28 @@ function changeColors(color){
     //assign colors
         squares[i].style.backgroundColor = color;
     }
+}
+
+function pickedColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num){
+    var arr = [];
+
+    for(var i = 0; i < num; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor(){
+    // pick r value, g value, b value
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+
 }
